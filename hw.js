847a40cb078d4783eb19197f00e12ee506ef5011
pranu88 <pranusha.pothugunta@gmail.com>
2023-10-13 15:34:03 -0400

    hw Started

--- hw.js
+++ hw.js
@@ -5,13 +5,91 @@
 // - If students get 55 or above: console log Not to good
 // // - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
 
+//  function grade (){
+//  let marks = 56;
+//     if(marks >= 55 && marks < 60){
+//         console.log('Not too good');
+//     }else if (marks >= 60 && marks < 70){
+//         console.log("ehhhhh");
+//      }else if(marks >=70 && marks < 80){
+//         console.log('Keep Trying');
+//       }else if(marks >= 80 && marks <= 100){
+//          console.log('You did a good job');
+//        }else if(marks > 100){
+//           console.log('Not valid number, Try again');
+//          }
+//      else{
+//          console.log("Bad grade but you'll get Em Next Time ");
+//         } 
+        
+//     } grade()
+
+
+
+
 
 // 2.   Write a function that prints out multiples of 10 up to a given input (argument)
+
+    // function multiples(j){
+
+    //     let i = 0 ;
+    //     while(i < j ){
+    //         i += 10 ;
+    //         console.log(i);
+    //     }
+    // }
+
+    // multiples(100)
+
+
 
 
 // 3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
 
+        // function change(billAmount, itemAmount){
+        //     let quarter = 0;
+        //     let remaining = billAmount - itemAmount;
+        //     while(remaining > 0){
+        //         remaining -= 0.25;
+        //         quarter++;                               
+                             
+        //     }  console.log(quarter);
+        // }     
+        // change (10, 8)   
 
 //4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  Return in as large denominations as possible. Parameters should be amount paind and amount cost
 
+
+     function bills(amountPaid, amountCost){
+      let change;
+      change = amountPaid - amountCost;
+      if(change > 0){
+      console.log(change + "$");
+      }else{
+        console.log("you should pay " + -(change) +"$" )
+      }
+     }
+     bills(10, 20)
+
+
+
+
 // 5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+// function FizzBuzz(){
+//     let i = 0;
+//     while(i <= 100){
+//         if(i % 3 ===0 && i % 5 !== 0){
+//             console.log("Fizz");
+//         }else if(i % 3 !== 0 && i % 5 === 0){
+//             console.log("Buzz");
+//         }else if(i % 3 === 0 && i % 5 ===0){
+//             console.log("FizzBuzz");
+//         }else{
+//             console.log(i);
+//         }
+//         i++;
+//     }
+
+// }FizzBuzz()
+
